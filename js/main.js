c34c16783abf35186a72af51a83fb2ad4b7cf2c3
mkzarra <mkzarra@gmail.com>
2018-06-03 10:25:43 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king"
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = (cards[0]);
+cardsInPlay.push(cardOne);
+cardsInPlay;
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+cardsInPlay;
 
 console.log("user flipped " + cardOne);
 console.log("user flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+  console.log("proceed");
+  if(cardsInPlay[0] === cardsInPlay[1]){
+    alert("You found a match");
+  }
+  else {
+    alert("Sorry, try again.");
+  }
+}
